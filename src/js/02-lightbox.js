47f4d549cd4e56ea89757b8e12635df31d352c57
raffamieczkowski <raffa.mieczkowski@gmail.com>
2023-03-28 22:47:49 +0200

    done
    done

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+for (const img of galleryItems) {
+  const listEl = `<div class="gallery__item">
+    <a class="gallery__link" href=${img.original}>
+      <img
+        class="gallery__image"
+        src=${img.preview}
+        data-source=${img.original}
+        alt=${img.description}
+      />
+    </a>
+  </div>`;
+  galleryEl.insertAdjacentHTML("beforeend", listEl);
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+  captionsData: "alt",
+});
